a1cdffa71b26b5fd807e252592f459ddc3ad4105
kentah <kenthow@gmail.com>
2015-09-27 15:40:33 -0500

    First and only commit

---
+++ config.js
@@ -0,0 +1,50 @@
+'use strict';
+
+exports.port = process.env.PORT || 3000;
+exports.mongodb = {
+  uri: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://kentah:abraxas73@ds051853.mongolab.com:51853/comevents'
+};
+exports.companyName = 'Community Events';
+exports.projectName = 'Community Events';
+exports.systemEmail = 'kenthow@gmail.com';
+exports.cryptoKey = 'k3yb0ardc4t';
+exports.loginAttempts = {
+  forIp: 50,
+  forIpAndUser: 7,
+  logExpiration: '20m'
+};
+exports.requireAccountVerification = false;
+exports.smtp = {
+  from: {
+    name: process.env.SMTP_FROM_NAME || exports.projectName +' Community Events',
+    address: process.env.SMTP_FROM_ADDRESS || 'kenthow@gmail.com'
+  },
+  credentials: {
+    user: process.env.SMTP_USERNAME || 'kenthow@gmail.com',
+    password: process.env.SMTP_PASSWORD || 'mandelbrot',
+    host: process.env.SMTP_HOST || 'smtp.gmail.com',
+    ssl: true
+  }
+};
+exports.oauth = {
+  twitter: {
+    key: process.env.TWITTER_OAUTH_KEY || '',
+    secret: process.env.TWITTER_OAUTH_SECRET || ''
+  },
+  facebook: {
+    key: process.env.FACEBOOK_OAUTH_KEY || '',
+    secret: process.env.FACEBOOK_OAUTH_SECRET || ''
+  },
+  github: {
+    key: process.env.GITHUB_OAUTH_KEY || '',
+    secret: process.env.GITHUB_OAUTH_SECRET || ''
+  },
+  google: {
+    key: process.env.GOOGLE_OAUTH_KEY || '',
+    secret: process.env.GOOGLE_OAUTH_SECRET || ''
+  },
+  tumblr: {
+    key: process.env.TUMBLR_OAUTH_KEY || '',
+    secret: process.env.TUMBLR_OAUTH_SECRET || ''
+  }
+};
